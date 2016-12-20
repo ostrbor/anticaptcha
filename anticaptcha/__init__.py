1e46021069d19103677a328f1b51769503c1964e
ostrbor <ostrbor@gmail.com>
2016-12-20 15:27:35 +0300

    + logging

--- anticaptcha/__init__.py
+++ anticaptcha/__init__.py
@@ -1,5 +1,7 @@
+import yaml
+import logging.config
 import requests
-from .settings import HEADERS, API_KEY
+from .settings import HEADERS, API_KEY, LOG_CONFIG
 from .exceptions import APIKeyMissingError
 
 if API_KEY is None:
@@ -7,5 +9,9 @@ if API_KEY is None:
 
 session = requests.Session()
 session.headers.update(HEADERS)
+
+with open(LOG_CONFIG) as config_file:
+    config = yaml.safe_load(config_file.read())
+    logging.config.dictConfig(config)
 
 from .anticaptcha import Anticaptcha
--- anticaptcha/anticaptcha.py
+++ anticaptcha/anticaptcha.py
@@ -1,3 +1,4 @@
+import logging
 from base64 import b64encode
 import json
 import time
@@ -11,25 +12,31 @@ class Anticaptcha:
     def __init__(self):
         self.clientKey = API_KEY
         self.base_url = BASE_URL
+        self.logger = logging.getLogger(__name__)
 
     def getBalance(self):
         """sends JSON data in POST request -> dict"""
+        self.logger.info('GET BALANCE')
         url = self.base_url + 'getBalance'
         data = {'clientKey': self.clientKey}
-        response = session.post(url, data=json.dumps(data))
-        return response.json()
+        response = session.post(url, data=json.dumps(data)).json()
+        self.logger.info('RESPONSE TO GET BALANCE: {}'.format(response))
+        return response
 
     def createTask(self, bin_str):
         """binary content of file -> id of task in dict"""
+        self.logger.info('CREATE TASK')
         url = self.base_url + 'createTask'
         img_str = b64encode(bin_str).decode('ascii')
         task = {'type': 'ImageToTextTask', 'body': img_str}
         data = {'clientKey': self.clientKey, 'task': task}
-        response = session.post(url, data=json.dumps(data))
-        return response.json()
+        response = session.post(url, data=json.dumps(data)).json()
+        self.logger.info('RESPONSE TO CREATE TASK: {}'.format(response))
+        return response
 
     def getTaskResult(self, task_id):
         """ -> dict with solution and extra info about task"""
+        self.logger.info('GET TASK RESULT')
         url = self.base_url + 'getTaskResult'
         time.sleep(WAIT_BEFORE_REQUESTS)
         total_sec = WAIT_BEFORE_REQUESTS
@@ -45,4 +52,5 @@ class Anticaptcha:
         else:
             raise TimeoutError('Spent {} seconds before giving up.'.format(
                 TIMEOUT))
+        self.logger.info('RESPONSE TO GET TASK RESULT: {}'.format(response))
         return response
