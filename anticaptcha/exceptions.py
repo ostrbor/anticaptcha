7898cbe16d468da94229a9737125eeb938bc1215
ostrbor <ostrbor@gmail.com>
2017-01-20 15:08:14 +0300

    Simplify exceptions

--- anticaptcha/exceptions.py
+++ anticaptcha/exceptions.py
@@ -1,7 +1,2 @@
-class APIKeyMissingError(Exception):
-    """No api key in settings.py"""
-    pass
-
-class TimeoutError(Exception):
-    """ timeout of wait for result """
+class AnticaptchaException(Exception):
     pass
