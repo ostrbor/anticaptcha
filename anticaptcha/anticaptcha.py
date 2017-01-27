7477598f29875ca2b25d667030da4aaeec3bf442
ostrbor <ostrbor@gmail.com>
2017-01-27 12:10:51 +0300

    Improve logging

--- anticaptcha/anticaptcha.py
+++ anticaptcha/anticaptcha.py
@@ -13,17 +13,27 @@ WAIT_BETWEEN_REQUESTS = 1
 class Anticaptcha:
     def __init__(self, API_KEY):
         self.clientKey = API_KEY
+        # for logging to hide full key from saving in log file
+        self.clientKey_short = self.clientKey[-8:]
         self.base_url = 'http://api.anti-captcha.com/'
         self.logger = logging.getLogger(__name__)
 
     def getBalance(self):
         """sends JSON data in POST request -> dict"""
-        self.logger.info('[ANTICAPTCHA] GET BALANCE')
         url = self.base_url + 'getBalance'
         data = {'clientKey': self.clientKey}
         response = session.post(url, data=json.dumps(data)).json()
-        self.logger.info('[ANTICAPTCHA] RESPONSE TO GET BALANCE: {}'.format(
-            response))
+        msg = "Sent [Get Balance] to url: %s with clientKey: %s" % (
+            url, self.clientKey_short)
+        self.logger.info(msg)
+        if response.get('errorCode'):
+            msg = "Received [Get Balance] error %s: %s" % (
+                response['errorCode'], response.get('errorDescription'))
+            self.logger.error(msg)
+        else:
+            msg = "Received [Get Balance] response: balance = %s" % (
+                response['balance'])
+            self.logger.info(msg)
         return response
 
     def createTask(self, bin_str):
@@ -33,27 +43,46 @@ class Anticaptcha:
         img_str = b64encode(bin_str).decode('ascii')
         task = {'type': 'ImageToTextTask', 'body': img_str}
         data = {'clientKey': self.clientKey, 'task': task}
+        msg = "Sent [Create Task] to url: %s with clientKey: %s" % (
+            url, self.clientKey_short)
+        self.logger.info(msg)
         response = session.post(url, data=json.dumps(data)).json()
-        self.logger.info('RESPONSE TO CREATE TASK: {}'.format(response))
+        if response.get('errorCode'):
+            msg = "Received [Create Task] error %s: %s" % (
+                response['errorCode'], response.get('errorDescription'))
+            self.logger.error(msg)
+        else:
+            msg = "Received [Create Task] response: taskId = %s" % (
+                response['taskId'])
+            self.logger.info(msg)
         return response
 
     def getTaskResult(self, task_id):
         """ -> dict with solution and extra info about task OR None"""
-        self.logger.info('GET TASK RESULT')
         url = self.base_url + 'getTaskResult'
         time.sleep(WAIT_BEFORE_REQUESTS)
         total_sec = WAIT_BEFORE_REQUESTS
         data = {'clientKey': self.clientKey, 'taskId': task_id}
         while total_sec <= TIMEOUT:
             response = session.post(url, data=json.dumps(data)).json()
-            if response.get('status') == 'processing':
+            msg = "Sent [Get Task Result] to url: %s with clientKey: %s" % (
+                url, self.clientKey_short)
+            self.logger.info(msg)
+            if response.get('errorCode'):
+                msg = "Received [Get Task Result] error %s: %s" % (
+                    response['errorCode'], response.get('errorDescription'))
+                self.logger.error(msg)
+                break
+            elif response.get('status') == 'processing':
                 time.sleep(WAIT_BETWEEN_REQUESTS)
                 total_sec += WAIT_BETWEEN_REQUESTS
                 continue
-            else:
-                break
+            elif response.get('status') == 'ready':
+                msg = "Received [Get Task Result] response: solution %s in %s seconds" % (
+                    response['solution']['text'], total_sec)
+                self.logger.info(msg)
+                return response
         else:
-            return
-        self.logger.info(
-            '[ANTICAPTCHA] RESPONSE TO GET TASK RESULT: {}'.format(response))
-        return response
+            msg = "Not solved Task (%s) after %s seconds of waiting" % (
+                task_id, total_sec)
+            self.logger.error(msg)
