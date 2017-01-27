938cc4e21afa8c71bd0a24b50dab8676534e4ede
ostrbor <ostrbor@gmail.com>
2017-01-27 12:45:52 +0300

    Refactor functions

--- anticaptcha/anticaptcha.py
+++ anticaptcha/anticaptcha.py
@@ -18,71 +18,68 @@ class Anticaptcha:
         self.base_url = 'http://api.anti-captcha.com/'
         self.logger = logging.getLogger(__name__)
 
-    def getBalance(self):
-        """sends JSON data in POST request -> dict"""
-        url = self.base_url + 'getBalance'
-        data = {'clientKey': self.clientKey}
+    def _send_post_request(self, url, data, api_method):
+        "Send request and log request"
         response = session.post(url, data=json.dumps(data)).json()
-        msg = "Sent [Get Balance] to url: %s with clientKey: %s" % (
-            url, self.clientKey_short)
+        msg = "Sent [%s] to url: %s with clientKey: %s" % (
+            api_method, url, self.clientKey_short)
         self.logger.info(msg)
+        return response
+
+    def _log_response(self, response, attr_to_log, api_method):
         if response.get('errorCode'):
-            msg = "Received [Get Balance] error %s: %s" % (
-                response['errorCode'], response.get('errorDescription'))
+            msg = "Received [%s] error %s: %s" % (
+                api_method, response['errorCode'],
+                response.get('errorDescription'))
             self.logger.error(msg)
         else:
-            msg = "Received [Get Balance] response: balance = %s" % (
-                response['balance'])
+            msg = "Received [%s] response: %s = %s" % (api_method, attr_to_log,
+                                                       response[attr_to_log])
             self.logger.info(msg)
-        return response
+
+    def getBalance(self):
+        """sends JSON data in POST request -> dict"""
+        api_method = 'getBalance'
+        url = self.base_url + api_method
+        data = {'clientKey': self.clientKey}
+        response = self._send_post_request(url, data, api_method)
+        self._log_response(response, 'balance', api_method)
 
     def createTask(self, bin_str):
         """binary content of file -> id of task in dict"""
-        self.logger.info('[ANTICAPTCHA] CREATE TASK')
-        url = self.base_url + 'createTask'
+        api_method = 'createTask'
+        url = self.base_url + api_method
         img_str = b64encode(bin_str).decode('ascii')
         task = {'type': 'ImageToTextTask', 'body': img_str}
         data = {'clientKey': self.clientKey, 'task': task}
-        msg = "Sent [Create Task] to url: %s with clientKey: %s" % (
-            url, self.clientKey_short)
-        self.logger.info(msg)
-        response = session.post(url, data=json.dumps(data)).json()
-        if response.get('errorCode'):
-            msg = "Received [Create Task] error %s: %s" % (
-                response['errorCode'], response.get('errorDescription'))
-            self.logger.error(msg)
-        else:
-            msg = "Received [Create Task] response: taskId = %s" % (
-                response['taskId'])
-            self.logger.info(msg)
-        return response
+        response = self._send_post_request(url, data, api_method)
+        self._log_response(response, 'taskId', api_method)
 
     def getTaskResult(self, task_id):
         """ -> dict with solution and extra info about task OR None"""
-        url = self.base_url + 'getTaskResult'
+        api_method = 'getTaskResult'
+        url = self.base_url + api_method
         time.sleep(WAIT_BEFORE_REQUESTS)
         total_sec = WAIT_BEFORE_REQUESTS
         data = {'clientKey': self.clientKey, 'taskId': task_id}
         while total_sec <= TIMEOUT:
-            response = session.post(url, data=json.dumps(data)).json()
-            msg = "Sent [Get Task Result] to url: %s with clientKey: %s" % (
-                url, self.clientKey_short)
-            self.logger.info(msg)
+            response = self._send_post_request(url, data, api_method)
             if response.get('errorCode'):
-                msg = "Received [Get Task Result] error %s: %s" % (
-                    response['errorCode'], response.get('errorDescription'))
+                msg = "Received [%s] error %s: %s" % (
+                    api_method, response['errorCode'],
+                    response.get('errorDescription'))
                 self.logger.error(msg)
-                break
+                return
             elif response.get('status') == 'processing':
                 time.sleep(WAIT_BETWEEN_REQUESTS)
                 total_sec += WAIT_BETWEEN_REQUESTS
                 continue
             elif response.get('status') == 'ready':
-                msg = "Received [Get Task Result] response: solution %s in %s seconds" % (
-                    response['solution']['text'], total_sec)
+                msg = "Received [%s] response: solution %s in %s seconds" % (
+                    api_method, response['solution']['text'], total_sec)
                 self.logger.info(msg)
                 return response
         else:
-            msg = "Not solved Task (%s) after %s seconds of waiting" % (
+            msg = "TIMEOUT. Task (%s) was not solved for %s seconds" % (
                 task_id, total_sec)
             self.logger.error(msg)
